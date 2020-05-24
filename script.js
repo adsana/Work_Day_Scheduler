f329d411106b11f99b82dac00b3d8571f8eab020
Adsana <adsanasayasith@gmail.com>
2020-05-24 04:02:50 -0400

    added rows and container

--- script.js
+++ script.js
@@ -4,3 +4,24 @@ function getLocalStorage(key) {
         $(`#text${key}`).text(value);
     }
 }
+
+$( document ).ready(function() {
+    $("#currentDay").text(moment().format("dddd, MMMM Do"));
+    for (let i = 9; i < 18; i++) {
+    
+        var row = $(`<div data-time=${i} id='${i}' class="row">`);
+
+        var col1 = $('<div class="col-sm-2"> <p class="hour">' + formatAMPM(i) + '</p>');
+
+        var col2 = $(`<div class="col-sm-8 past"><textarea id=text${i} class="description" placeholder="Add your event here..."></textarea>`);        
+       
+        var col3 = $(`<div class="col-sm-2"><button class="saveBtn" id=${i}><i class="fas fa-save"></i></button>`)
+        
+        row.append(col1);
+        row.append(col2);
+        row.append(col3);
+
+        $(".container").append(row);
+
+        getLocalStorage(i);
+    }
